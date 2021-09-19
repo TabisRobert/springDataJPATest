bc122cefa98aaa32bae7e3c36426157b34704b84
robert.tabis <robert.tabis@hotmail.com>
2021-09-19 11:29:46 +0200

    OrderRepository created

--- src/main/java/com/motorola/springDataJPA/OrderRepository.java
+++ src/main/java/com/motorola/springDataJPA/OrderRepository.java
@@ -2,7 +2,8 @@ package com.motorola.springDataJPA;
 
 import org.springframework.data.repository.CrudRepository;
 
+
 public interface OrderRepository extends CrudRepository<Order, Long> {
 
-    
+    Order findByOrderId(Long orderId);
 }
